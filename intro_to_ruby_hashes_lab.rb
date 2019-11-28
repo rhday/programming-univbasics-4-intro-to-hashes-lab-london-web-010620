babe466f02d6432d3aa630036aa7df3f6ba643fa
Ruari Day <ruariday@gmail.com>
2019-11-28 09:31:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ def id_generator
   id_generator = {id: 2}
 end
 
-def my_hash_creator(:name, "Grace Hopper")
+def my_hash_creator(key,value)
   my_hash_creator = {:name =>"Grace Hopper"}
   return my_hash_creator
 end
